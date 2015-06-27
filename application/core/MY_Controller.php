0fb4c2e739e9643ddca0def01457a4e5682e7e71
manikandan_ <s_mani@outlook.com>
2015-06-28 00:27:53 +0530

    uncommented redirection

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,7 +5,7 @@ class MY_Controller extends CI_Controller {
 		parent::__construct();
 		$user_id = $this->session->userdata('user_id');
 		if(empty($user_id)) {
-//			redirect('login');die;
+			redirect('login');die;
 		}
 		global $data;
 		$data['user_name'] 	= $this->session->userdata('user_name');
